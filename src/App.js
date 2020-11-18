9549d44982dc020849c82894080bdc02a956173a
nag422 <nagendrakumar422@gmail.com>
2020-11-19 01:34:59 +0530

    final react push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 // import './App.css';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
-
-
 import { getPosts } from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts'
@@ -11,29 +9,33 @@ import Form from './components/Form/Form'
 import useStyles from './styles';
 
 const App = () => {
+  const [currentId, setCurrentId] = useState(0);
   const classes = useStyles();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPosts());
     
-  }, [dispatch])
+    
+  }, [currentId,dispatch])
 
 
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories<img className={classes.image} src={memories} alt="memories" height="60" /></Typography>
+        <Typography className={classes.heading} variant="h2" align="center">Memories
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+        </Typography>
         
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems = "stretch" spacing="3">
+          <Grid container className={classes.mainContainer} justify="space-between" alignItems = "stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-                <Posts />
+                <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-                <Form />
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
